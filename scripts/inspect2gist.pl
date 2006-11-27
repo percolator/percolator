3702dfafad711de1e385dc1feed754ac9129e403
lukall <lukall>
2006-11-27 03:24:28 +0000

    *** empty log message ***

--- scripts/inspect2gist.pl
+++ scripts/inspect2gist.pl
@@ -23,8 +23,8 @@ my $line = <FH>; # skipp annotation line
 print "Id\tCutScore\tp-value\tDeltaCN\tMQScore\tDeltaCnOther\tpepLen\tz1\tz2\tz3\ttrypN\ttrypC\n";
 while ($line=<FH>) {
     chomp $line;
-    my @field = split /\t/,$line;
-    my @of = map {$_->[0].'_'.$_->[1].$_->[4] $_->[6] $_->[10] $_->[11] $->[5] $_->[12] chargeVec($_->[4]) isTrypN($_->[2]) isTrypC($_->[2])}  @field;
+    split /\t/,$line;
+    my @of = ($_[0] . '_' . $_[1] . $_[4], $_[6], $_[10], $_[11], $_[5], $_[12], chargeVec($_->[4]),isTrypN($_->[2]), isTrypC($_->[2]));
     print join('\t',@of) . "\n";
 }
 close(*FH);
