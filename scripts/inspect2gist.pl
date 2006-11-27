678f7e82cddf1080d13a9917cf2522a97c3cca38
lukall <lukall>
2006-11-27 04:46:21 +0000

    *** empty log message ***

--- scripts/inspect2gist.pl
+++ scripts/inspect2gist.pl
@@ -4,6 +4,9 @@
 #0            1     2          3       4      5       6        7         8         9       10      11      12           13           14        15
 #SpectrumFile Scan# Annotation Protein Charge MQScore CutScore IntenseBY BYPresent Unused  p-value DeltaCN DeltaCNOther RecordNumber DBFilePos SpecFilePos
 
+use strict;
+local *IN,*DATA,*LABEL;
+
 sub isTrypN {
     my $seq = shift;
     return (($seq =~ m/^[A-Z]*[KR][.][^P]/g) || ($seq =~ m/^[*][.]/g))?1:0;
@@ -19,16 +22,33 @@ sub chargeVec {
     return ($charge==1?1:0,$charge==2?1:0,$charge==3?1:0);
 }
 
-open(*FH,"< " . shift ARGV);
-my $line = <FH>; # skipp annotation line
-print "Id\tCutScore\tp-value\tDeltaCn\tMQScore\tDeltaCnOther\tpepLen\tz1\tz2\tz3\ttrypN\ttrypC\n";
-my $oldId = "xx";
-while ($line=<FH>) {
-    chomp $line;
-    split /\t/,$line;
-    my $id =  $_[0] . '_' . $_[1] . '_' . $_[4];
-    my @of = ($id, $_[6], $_[10], $_[11], $_[5], $_[12],length($_[2])-4 ,chargeVec($_[4]),isTrypN($_[2]), isTrypC($_[2]));
-    print join("\t",@of) . "\n" if ($id ne $oldId);
-    $oldId=$id;
+sub convert {
+  my $label = shift;
+  my $line = <IN>; # skipp annotation line
+  print "Id\tCutScore\tp-value\tDeltaCn\tMQScore\tDeltaCnOther\tpepLen\tz1\tz2\tz3\ttrypN\ttrypC\n";
+  my $oldId = "xx";
+  while ($line=<IN>) {
+      chomp $line;
+      split /\t/,$line;
+      my $id =  $_[0] . '_' . $_[1] . '_' . $_[4];
+      my @of = ($id, $_[6], $_[10], $_[11], $_[5], $_[12],length($_[2])-4 ,chargeVec($_[4]),isTrypN($_[2]), isTrypC($_[2]));
+      print DATA join("\t",@of) . "\n" if ($id ne $oldId);
+      print LABEL "$id\t$label\n"; if ($id ne $oldId);
+      $oldId=$id;
+  }
 }
-close(*FH);
+
+open(*DATA,"> " .shift ARGV);
+print DATA "Id\tCutScore\tp-value\tDeltaCn\tMQScore\tDeltaCnOther\tpepLen\tz1\tz2\tz3\ttrypN\ttrypC\n";
+open(*LABEL,"> " .shift ARGV);
+print LABEL "Id\tLabel\n";
+open(*IN,"< " . shift ARGV);
+convert("1");
+close(*IN);
+open(*IN,"< " . shift ARGV);
+convert("-1");
+close(*IN);
+close(*LABEL);
+close(*DATA);
+
+
