5caa97e2b1cc5dfeb2af02cf6005ba91a54c686c
lukall <lukall>
2007-02-05 22:12:24 +0000

    *** empty log message ***

---
+++ src/Release/subdir.mk
@@ -0,0 +1,54 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../Caller.cpp \
+../DataSet.cpp \
+../Globals.cpp \
+../IntraSetRelation.cpp \
+../Normalizer.cpp \
+../Option.cpp \
+../Scores.cpp \
+../SetHandler.cpp \
+../StdvNormalizer.cpp \
+../UniNormalizer.cpp \
+../ssl.cpp 
+
+OBJS += \
+./Caller.o \
+./DataSet.o \
+./Globals.o \
+./IntraSetRelation.o \
+./Normalizer.o \
+./Option.o \
+./Scores.o \
+./SetHandler.o \
+./StdvNormalizer.o \
+./UniNormalizer.o \
+./ssl.o 
+
+CPP_DEPS += \
+./Caller.d \
+./DataSet.d \
+./Globals.d \
+./IntraSetRelation.d \
+./Normalizer.d \
+./Option.d \
+./Scores.d \
+./SetHandler.d \
+./StdvNormalizer.d \
+./UniNormalizer.d \
+./ssl.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+%.o: ../%.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -O3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o"$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
