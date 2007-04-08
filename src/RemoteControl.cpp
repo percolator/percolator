9a5c190122d7a5466cab74ee6f25fcc8b1131895
lukall <lukall>
2007-04-08 22:06:45 +0000

    *** empty log message ***

--- src/RemoteControl.cpp
+++ src/RemoteControl.cpp
@@ -1,9 +1,12 @@
 #include "RemoteControl.h"
 
-RemoteControl::RemoteControl()
-{
-}
+void initiate(NSet sets, unsigned int numOfFeatures, unsigned int numOfSpectra, char ** featureNames) {;}
 
-RemoteControl::~RemoteControl()
-{
-}
+/* Register a PSM */
+void registerPSM(SetType set, char * identifier, double * features) {;}
+
+/* Function called after all features are registered */
+void endRegistration() {;}
+
+/* Function called when we whant to start processing */
+void go() {;} 
