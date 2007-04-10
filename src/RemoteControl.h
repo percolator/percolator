477d9a7b5399f3b55c9eb302fba2324782469957
lukall <lukall>
2007-04-10 05:11:43 +0000

    Bills comments

--- src/RemoteControl.h
+++ src/RemoteControl.h
@@ -4,8 +4,14 @@
 extern "C" {
 #endif
 
-typedef enum {TWO_SETS,THREE_SETS,FOUR_SETS} NSet;
-typedef enum {TARGET,DECOY,TEST_DECOY,THRESHOLD_DECOY} SetType;
+/* Number of target and decoy sets that external program will hand over to percolator.
+ * The value should correspond to the number of sequence databases that have been searched.
+ * Percolators validation strategy will be the same as for the stand alone version given the
+ * corresponding number of sqt-files as input. */
+typedef enum {TWO_SETS=2,THREE_SETS,FOUR_SETS} NSet;
+
+/* Identifying which set the PSM belongs to*/
+typedef enum {TARGET=0,DECOY1,DECOY2,DECOY3} SetType;
 
 /* Call that initiates percolator */
 void initiate(NSet sets, unsigned int numOfFeatures, unsigned int numOfSpectra, char ** featureNames);
@@ -16,13 +22,10 @@ void registerPSM(SetType set, char * identifier, double * features);
 /* Function called after all features are registered */
 void endRegistration();
 
-/* Function called when we whant to start processing */
+/* Function called when we want to start processing */
 void go(); 
 
 #ifdef __cplusplus
 }
 #endif
-
-
-
 #endif /*REMOTECONTROL_H_*/
