99032b11a69dd425e3b2a7b5ea64a2a104f3556f
lukall <lukall>
2007-07-17 21:34:54 +0000

    *** empty log message ***

--- src/PercolatorCInterface.h
+++ src/PercolatorCInterface.h
@@ -19,21 +19,13 @@ void pcInitiate(NSet sets, unsigned int numFeatures, unsigned int numSpectra, ch
 /** Register a PSM */
 void pcRegisterPSM(SetType set, char * identifier, double * features);
 
-/** Function called after all features are registered */
-void pcEndRegistration();
-
 /** Function called when we want to start processing */
 void pcExecute(); 
 
-/** Function called when retrieving target q-values after processing,
+/** Function called when retrieving target scores and q-values after processing,
   * the array should be numSpectra long and will be filled in the same order
   * as the features were inserted */
-void pcGetQ(double *qArr); 
-
-/** Function called when retrieving target scores after processing,
-  * the array should be numSpectra long and will be filled in the same order
-  * as the features were inserted */
-void pcGetScores(double *scoreArr); 
+void pcGetScores(double *scoreArr, double *qArr); 
 
 /** Function that should be called after processing finished */
 void pcCleanUp(); 
