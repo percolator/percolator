6577f7b192b21c9366ccd97f18f108dbabeb5080
lukall <lukall>
2009-02-12 14:31:47 +0000

    *** empty log message ***

--- src/LogisticRegression.cpp
+++ src/LogisticRegression.cpp
@@ -11,8 +11,8 @@
  conditions:
 
  The above copyright notice and this permission notice shall be
- included in all copies or substantial portions of the Software. 
- 
+ included in all copies or substantial portions of the Software.
+
  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
  OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
@@ -21,9 +21,9 @@
  WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
  OTHER DEALINGS IN THE SOFTWARE.
- 
- $Id: LogisticRegression.cpp,v 1.6 2009/01/09 14:40:59 lukall Exp $
- 
+
+ $Id: LogisticRegression.cpp,v 1.7 2009/02/12 14:31:47 lukall Exp $
+
  *******************************************************************************/
 #include<iterator>
 #include<vector>
@@ -66,16 +66,23 @@ void LogisticRegression::calcPZW() {
     p[ix] = min(max(e/(1+e),Numerical::epsilon),1-Numerical::epsilon);
     w[ix] = m[ix]*p[ix]*(1-p[ix]);
     z[ix] = g[ix] + (((double)y[ix])-p[ix]*((double)m[ix]))/w[ix];
+    assert(isfinite(g[ix]));
+    assert(isfinite(e));
+    assert(isfinite(p[ix]));
+    assert(isfinite(w[ix]));
+    assert(isfinite(z[ix]));
   }
 }
 
 void LogisticRegression::initg() {
   BaseSpline::initg();
   int n=x.size();
-  p.resize(n);  
+  p.resize(n);
   for (int ix=g.size();ix--;) {
-    double p = (y[ix]+0.1)/(m[ix]+0.1);
+    double p = (y[ix]+0.05)/(m[ix]+0.1);
     gnew[ix] = log(p/(1-p));
+    assert(isfinite(p));
+    assert(isfinite(g[ix]));
   }
 //#define OUTPUT_DEBUG_FILES
 #undef OUTPUT_DEBUG_FILES
@@ -84,11 +91,11 @@ void LogisticRegression::initg() {
   ostream_iterator<double> xvalIt(xvalFile,"\n");
 
   copy(x.begin(),x.end(),xvalIt);
-  
+
   for(size_t yix=0;yix<y.size();++yix) {
     drFile << y[yix]/(double) m[yix] << endl;
   }
-  drFile.close();  
+  drFile.close();
 #endif
 }
 
