c457622854ef1f0fb0ad4a8cd9a8befd8a5d96d4
Erik Sjölund <erik.sjolund@gmail.com>
2010-06-16 15:02:59 +0200

    Adjusting build_me.sh. We should not build in the source directory.

--- build_me.sh
+++ build_me.sh
@@ -1,9 +1,27 @@
-echo 'Creating out-of-source output folder...'
-mkdir output
-cd output
-echo 'CMaking...'
-cmake -DCMAKE_BUILD_TYPE=Release -DGOOGLE_TEST=FALSE ..
-echo 'Building...'
-make
-echo 'Installing...'
-sudo make install
+#!/bin/sh
+
+
+
+rm -rf /tmp/build && mkdir /tmp/build && cd /tmp/build  && ~/cmake-2.8.1-Linux-i386/bin/cmake -DCMAKE_INSTALL_PREFIX=/tmp/install '-DCMAKE_PREFIX_PATH=/scratch/esjolund/xsd-3.3.0-x86_64-linux-gnu;/scratch/esjolund/xerces-c-3.1.1/inst/;/scratch/esjolund/xsd-3.3.0-x86_64-linux-gnu/;/scratch/esjolund/tokyocabinet-1.4.44/inst/' /scratch/e/nypercol/percolator/
+
+export PKG_CONFIG_PATH=/scratch/esjolund/xerces-c-3.1.1/inst/lib/pkgconfig/
+export PATH=/scratch/esjolund/xerces-c-3.1.1/inst/bin/:$PATH
+export LD_LIBRARY_PATH=/scratch/esjolund/xerces-c-3.1.1/inst/lib/:$LD_LIBRARY_PATH
+
+# as soon as a command fails terminate this script
+set -e
+
+percolatorSourceDir=`/bin/pwd`
+buildDir=/tmp/percolatorBuildDir
+installDir=/tmp/percolatorInstallDir
+cd $buildDir 
+
+
+# you might want to add something like
+#'-DCMAKE_PREFIX_PATH=/scratch/esjolund/xsd-3.3.0-x86_64-linux-gnu;/scratch/esjolund/xerces-c-3.1.1/inst/;/scratch/esjolund/xsd-3.3.0-x86_64-linux-gnu/;/scratch/esjolund/tokyocabinet-1.4.44/inst/'
+# 
+
+cmake -DCMAKE_BUILD_TYPE=Release -DGOOGLE_TEST=FALSE DCMAKE_INSTALL_PREFIX=$installDir  $percolatorSourceDir
+make -j8
+
+
