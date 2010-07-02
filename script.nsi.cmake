a8f7cc24154e61a9739d9a1369246673aa509428
Erik Sjölund <erik.sjolund@gmail.com>
2010-07-02 19:45:32 +0200

    mingw build functionality is in process

---
+++ script.nsi.cmake
@@ -0,0 +1,35 @@
+!include "MUI2.nsh"
+Name "percolator-@PACKAGE_VERSION@"
+outfile "@CMAKE_BINARY_DIR@/percolator-@PACKAGE_VERSION@-win32.exe"
+installDir $PROGRAMFILES\percolator
+
+!define MUI_ABORTWARNING
+
+!insertmacro MUI_PAGE_LICENSE "@CMAKE_SOURCE_DIR@/license-win32-statically-built"
+; !insertmacro MUI_PAGE_COMPONENTS 
+!insertmacro MUI_PAGE_DIRECTORY
+!insertmacro MUI_PAGE_INSTFILES
+ 
+!insertmacro MUI_UNPAGE_CONFIRM
+!insertmacro MUI_UNPAGE_INSTFILES
+  
+!insertmacro MUI_LANGUAGE "English"
+
+Section "Dummy Section" SecDummy
+  SetOutPath "$INSTDIR"
+  file src/percolator.exe
+# I had problems installing Tokyo Cabinet and XDR on Windows /Erik
+#  file src/converters/sqt2pin.exe
+#  file src/converters/mzidentml2pin.exe
+  file src/elude/elude.exe
+  WriteUninstaller "$INSTDIR\Uninstall.exe"
+SectionEnd
+
+Section "Uninstall"
+  Delete "$INSTDIR\Uninstall.exe"
+  Delete "$INSTDIR\percolator.exe"
+#  Delete "$INSTDIR\sqt2pin.exe"
+#  Delete "$INSTDIR\mzidentml2pin.exe"
+  Delete "$INSTDIR\elude.exe"
+  RMDir "$INSTDIR"
+SectionEnd
