1193dec89c494eb9924fa8d08bd549555e827713
mattia <mattia@dirac.(none)>
2010-08-26 17:31:50 +0200

    fixed path problems within SystemTest_Percolator_Performance.py: tests now succeeds from Cmake!

--- tests/SystemTest_Percolator_Performance.py
+++ tests/SystemTest_Percolator_Performance.py
@@ -3,16 +3,19 @@
 # Parameters: none
 
 import os
+import sys
+
+path = os.path.dirname(sys.argv[0])
 
 # running sqt2pin to generate pin.xml
 print "PERCOLATOR PERFORMANCE (STEP 1): running sqt2pin..." 
-os.popen("./sqt2pin -x data/percolator_test/pin.xml data/percolator_test/target.sqt " 
-  + "data/percolator_test/reverse.sqt")
+os.popen(path + "/sqt2pin -x " + path + "/data/percolator_test/pin.xml " + path 
+  + "/data/percolator_test/target.sqt " + path + "/data/percolator_test/reverse.sqt")
 
 # running percolator on pin.xml; the output line containing pi_0 is extracted
 # and if its value is outside of 622+/-5% an error is reported
 print "PERCOLATOR PERFORMANCE (STEP 2): running percolator..."
-output = os.popen("./percolator data/percolator_test/pin.xml " + " 2>&1 | grep \"New pi_0 estimate\"")
+output = os.popen(path + "/percolator " + path + "/data/percolator_test/pin.xml 2>&1 | grep \"New pi_0 estimate\"")
 outputStr = output.read()
 # extracting pi_0 value
 pi_0 = int(outputStr[39:42])
