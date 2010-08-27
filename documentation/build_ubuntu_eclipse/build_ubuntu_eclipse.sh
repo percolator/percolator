0e884016583807c80b96363a43fa297a58dc9dbe
sphereworld <sphereworld@sphereworld-laptop.(none)>
2010-08-27 13:19:25 +0200

    got rid of configuration parameters in build_ubuntu_eclipse-sh

--- documentation/build_ubuntu_eclipse/build_ubuntu_eclipse.sh
+++ documentation/build_ubuntu_eclipse/build_ubuntu_eclipse.sh
@@ -3,73 +3,71 @@
 # Script that builds Percolator as an Eclipse Project
 # Parameters: none
 
-###############################
-# USER MUST SET THESE VARIABLES
-###############################
-home=/home/mattia # location where the percolator source from the repositories will be stored
-buildDir=/home/mattia/percolatorBuild # location where it will be built
-installDir=/home/mattia/percolatorBuild # location where it will be installed
-codesynthesisDir=/home/mattia/codesynthesis # location for the cosesynthesis library
+src=$(pwd)
+buildDir=$src/percolatorBuild # location where it will be built
+installDir=$src
+
 
 # install necessary libraries
 set -e # as soon as a command fails terminate this script
-sudo apt-get install libxerces-c-dev libboost-dev gengetopt libsqlite3-dev cmake libtokyocabinet-dev
+sudo apt-get install libxerces-c-dev libboost-dev gengetopt libsqlite3-dev cmake libtokyocabinet-dev git-core zlib1g zlib1g-dev build-essential
 
 
-# clean old runs of this script
-echo ""
-echo "**********"
-echo "STEP 1: clean old runs..." 
-echo "**********"
-if [ -d "$codesynthesisDir" ]; then
-    rm -r $codesynthesisDir
-fi
-if [ -d "$buildDir" ]; then
-    rm -r $buildDir
-fi
-if [ -d "$installDir" ]; then
-    rm -r $installDir
-fi
-if [ -d "$home/percolator" ]; then
-    rm -r -f $home/percolator
+# get codesynthesis
+echo "*******"
+echo "STEP 1: installing codesynthesis library..." 
+echo "*******"
+cd $src
+if [ ! -d "xsd-3.3.0-x86_64-linux-gnu" ]; then
+    wget http://codesynthesis.com/download/xsd/3.3/linux-gnu/x86_64/xsd-3.3.0-x86_64-linux-gnu.tar.bz2
+    tar xfj xsd-3.3.0-x86_64-linux-gnu.tar.bz2
+    rm xsd-3.3.0-x86_64-linux-gnu.tar.bz2
 fi
 echo "STEP 1 DONE."
 echo ""
 
 
-# get codesynthesis
-echo "**********"
-echo "STEP 2: codesynthesis..." 
-echo "**********"
-mkdir $codesynthesisDir
-cd $codesynthesisDir
-wget http://codesynthesis.com/download/xsd/3.3/linux-gnu/x86_64/xsd-3.3.0-x86_64-linux-gnu.tar.bz2
-tar xfj $codesynthesisDir/xsd-3.3.0-x86_64-linux-gnu.tar.bz2
+echo "*******"
+echo "STEP 2: clone/pull percolator from repositories..." # get percolator
+echo "*******"
+cd $src
+if [ ! -d "percolator" ]; then
+    git clone git@github.com:percolator/percolator.git
+else
+    cd percolator
+    git pull
+    cd ..
+fi
 echo "STEP 2 DONE."
 echo ""
 
 
-echo "**********"
-echo "STEP 3: get percolator from repositories..." # get percolator
-echo "**********"
-cd $home
-git clone git@github.com:percolator/percolator.git
+echo "*******"
+echo "STEP 3 running cmake..." # run cmake with Eclipse CDT4 option
+echo "*******"
+cd $src
+echo $src
+if [ -d "$buildDir" ]; then
+   rm -r $buildDir
+fi
 mkdir $buildDir
-mkdir $installDir
+cd $buildDir
+cmake -G"Eclipse CDT4 - Unix Makefiles" -D CMAKE_BUILD_TYPE=Debug -DGOOGLE_TEST=FALSE -DCMAKE_INSTALL_PREFIX=$installDir -DCMAKE_PREFIX_PATH=$src/xsd-3.3.0-x86_64-linux-gnu/ $src/percolator
 echo "STEP 3 DONE."
 echo ""
 
 
-echo "**********"
-echo "STEP 4 running cmake..." # run cmake with Eclipse CDT4 option
-echo "**********"
+echo "*******"
+echo "STEP 4 building, installing and testing..." # run cmake with Eclipse CDT4 option
+echo "*******"
 cd $buildDir
-cmake -G"Eclipse CDT4 - Unix Makefiles" -D CMAKE_BUILD_TYPE=Debug -DGOOGLE_TEST=TRUE -DCMAKE_INSTALL_PREFIX=$installDir -DCMAKE_PREFIX_PATH=$codesynthesisDir/xsd-3.3.0-x86_64-linux-gnu/ $home/percolator
+make
+make install
+make test
 echo "STEP 4 DONE."
 echo ""
 
-
-echo "**********"
-echo "SUCCESS!" $buildDir "contains a valid Eclipse Percolator project."
-echo "**********"
+echo "*******"
+echo "SUCCESS!" $buildDir "contains a valid Eclipse project."
+echo "*******"
 echo ""
