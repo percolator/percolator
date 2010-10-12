75302d66cd4fb686691629696956ff4443584005
Mattia Tomasoni <tomasoni@dirac.dbb..su.se>
2010-10-12 12:12:01 +0200

    .

--- src/Exceptions.cpp
+++
@@ -1,7 +0,0 @@
-#include "Exceptions.h"
-
-const char* MS2Exception::what() const throw(){
-  return "test";
-  //return "The ms2 in input does not appear to contain retention time "
-  //    + "information. Please run without -2 option.";
-}
