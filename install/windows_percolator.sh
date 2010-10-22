866ee50b4f62ddbd8adf84b253e2573fb2eac29c
Mattia Tomasoni <tomasoni@dirac.dbb..su.se>
2010-10-22 15:03:02 +0200

    correction to window installer script

--- install/windows_percolator.sh
+++ install/windows_percolator.sh
@@ -47,9 +47,3 @@ cd $percolatorBuild
 cmake -DCMAKE_TOOLCHAIN_FILE=$percolatorSource/install/cmake/Toolchain-mingw32.cmake '-DCMAKE_PREFIX_PATH='$base'/xsd-3.3.0-i686-windows;'$base'/xsd-3.3.0-x86_64-linux-gnu' -DMINGW=ON -DSTATIC=ON -DGOOGLE_TEST=FALSE -DEXCLUDE_CONVERTERS=ON $percolatorSource
 
 make -j 8 win32installer
-make install
-
-PATH=$PATH:$percolatorInstall
-export PATH
-
-make test
