a0fff99b7a3346b680e398d65149343da669ab79
Mattia Tomasoni <tomasoni@dirac.dbb..su.se>
2010-10-25 15:58:38 +0200

    .

---
+++ src/FeatureNames.h
@@ -0,0 +1,115 @@
+/*******************************************************************************
+ Copyright 2006-2009 Lukas Käll <lukas.kall@cbr.su.se>
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+
+ *******************************************************************************/
+#ifndef FEATURENAMES_H_
+#define FEATURENAMES_H_
+
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <iostream>
+#include <assert.h>
+#include <boost/foreach.hpp>
+#include "percolator_in.hxx"
+
+using namespace std;
+
+class FeatureNames {
+  public:
+    FeatureNames();
+    virtual ~FeatureNames();
+    string getFeatureNames(bool skipDOC = false);
+    static inline size_t getNumFeatures() {
+      return numFeatures;
+    }
+    static inline void setNumFeatures(size_t nf) {
+      if (!numFeatures) {
+        numFeatures = nf;
+      }
+    }
+
+
+    void setFromXml( const ::percolatorInNs::featureDescriptions & fdes, bool calcDOC );
+
+    void insertFeature(const string& featureName) {
+      featureNames.push_back(featureName);
+    }
+    void setFeatures(string& line, size_t skip, size_t numFeatures);
+
+    int getFeatureNumber(const string& featureName) {
+      return distance(find(featureNames.begin(),
+                           featureNames.end(),
+                           featureName), featureNames.begin());
+    }
+    void setSQTFeatures(int minCharge, int maxCharge, bool doEnzyme,
+                        bool calcPTMs, bool doManyHitsPerSpectrum,
+                        const string& aaAlphabet,
+                        bool calQuadraticFeatures, bool calcDOC);
+    // SQT Feature Number getters, will return -1 if not defined.
+    int getMinCharge() {
+      return minCharge;
+    }
+    int getMaxCharge() {
+      return maxCharge;
+    }
+    int getChargeFeatNum() {
+      assert(1==0);
+      return chargeFeatNum;
+    }
+    int getEnzFeatNum() {
+      assert(1==0);
+      return enzFeatNum;
+    }
+    int getNumSPFeatNum() {
+      assert(1==0);
+      return numSPFeatNum;
+    }
+    int getPtmFeatNum() {
+      assert(1==0);
+      return ptmFeatNum;
+    }
+    int getPNGaseFFeatNum() {
+      assert(1==0);
+      return pngFeatNum;
+    }
+    int getAaFeatNum() {
+      assert(1==0);
+      return aaFeatNum;
+    }
+    int getIntraSetFeatNum() {
+      assert(1==0);
+      return intraSetFeatNum;
+    }
+    int getQuadraticFeatNum() {
+      assert(1==0);
+      return quadraticFeatNum;
+    }
+    int getDocFeatNum() {
+      return docFeatNum;
+    }
+    void setDocFeatNum(int fn) {
+      docFeatNum = fn;
+    }
+  protected:
+    vector<string> featureNames;
+    static size_t numFeatures;
+    int minCharge, maxCharge;
+    int chargeFeatNum, enzFeatNum, numSPFeatNum, ptmFeatNum, pngFeatNum,
+        aaFeatNum, intraSetFeatNum, quadraticFeatNum, docFeatNum;
+};
+
+#endif /*FEATURENAMES_H_*/
---
+++ src/converters/SqtReader.h
@@ -0,0 +1,62 @@
+#ifndef SQTREADER_H
+#define SQTREADER_H
+
+#include <string>
+#include <set>
+#include <iostream>
+#include <fstream>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include "FragSpectrumScanDatabase.h"
+#include "Globals.h"
+#include "SqtReader.h"
+#include "Enzyme.h"
+#include "MassHandler.h"
+#include "DataSet.h"
+#include "FeatureNames.h"
+#include "percolator_in.hxx"
+using namespace std;
+
+class ParseOptions {
+  public:
+    ParseOptions(): calcQuadraticFeatures(false),
+    calcAAFrequencies(false),
+    calcPTMs(false),
+    isotopeMass(false),
+    pngasef(false),
+    calcDOC(false),
+    hitsPerSpectrum(1),
+    reversedFeaturePattern(""){};
+    bool calcQuadraticFeatures;
+    bool calcAAFrequencies;
+    bool calcPTMs;
+    bool calcDOC;
+    bool isotopeMass;
+    int hitsPerSpectrum;
+    bool pngasef;
+    std::string reversedFeaturePattern;
+  private:
+    string getRidOfUnprintables(string inpString);
+};
+
+namespace SqtReader {
+enum parseType { justSearchMaxMinCharge, fullParsing };
+
+
+void translateSqtFileToXML(const std::string fn,::percolatorInNs::featureDescriptions & fds, ::percolatorInNs::experiment::fragSpectrumScan_sequence  & fsss,   bool isDecoy, const ParseOptions & po, int * maxCharge,  int * minCharge, parseType pType, FragSpectrumScanDatabase & database  );
+void readSQT(const std::string fn,::percolatorInNs::featureDescriptions & fds, ::percolatorInNs::experiment::fragSpectrumScan_sequence  & fsss,  bool is_decoy, const ParseOptions & po, int * maxCharge,  int * minCharge, parseType t, FragSpectrumScanDatabase & database  );
+void addFeatureDescriptions( percolatorInNs::featureDescriptions & fe_des, int minC, int maxC, bool doEnzyme,
+    bool calcPTMs, bool doPNGaseF,
+    const std::string& aaAlphabet,
+    bool calcQuadratic);
+void readSectionS( std::string record, ::percolatorInNs::experiment::fragSpectrumScan_sequence & fsss, std::set<int> & theMs, bool is_decoy, const ParseOptions & po, int minCharge, int maxCharge, std::string psmId, FragSpectrumScanDatabase & database  );
+void readPSM( bool is_decoy, const std::string &in, int match, const ParseOptions & po, ::percolatorInNs::experiment::fragSpectrumScan_sequence & fsss, int minCharge, int maxCharge, std::string psmId, FragSpectrumScanDatabase & database );
+void push_backFeatureDescription( percolatorInNs::featureDescriptions::featureDescription_sequence  & fd_sequence, const char *);
+void computeAAFrequencies(const string& pep,   percolatorInNs::features::feature_sequence & f_seq );
+}
+
+#endif
+
+
+
