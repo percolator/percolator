8a802d653e9af56bf9bee063dfb1af87ee9d5b3e
Mattia Tomasoni <tomasoni@dirac.dbb..su.se>
2010-10-28 13:16:41 +0200

    temporarily excluding elude in win32 install script

--- install/windows_percolator.sh
+++ install/windows_percolator.sh
@@ -44,6 +44,6 @@ mkdir $percolatorInstall
 cd $percolatorBuild
 
 
-cmake -DCMAKE_TOOLCHAIN_FILE=$percolatorSource/install/cmake/Toolchain-mingw32.cmake '-DCMAKE_PREFIX_PATH='$base'/xsd-3.3.0-i686-windows;'$base'/xsd-3.3.0-x86_64-linux-gnu' -DMINGW=ON -DSTATIC=ON -DGOOGLE_TEST=FALSE -DEXCLUDE_CONVERTERS=ON -DEXCLUDE_ELUDE=ON $percolatorSource
+cmake -DCMAKE_TOOLCHAIN_FILE=$percolatorSource/install/cmake/Toolchain-mingw32.cmake '-DCMAKE_PREFIX_PATH='$base'/xsd-3.3.0-i686-windows;'$base'/xsd-3.3.0-x86_64-linux-gnu' -DMINGW=ON -DSTATIC=ON -DGOOGLE_TEST=FALSE -DEXCLUDE_CONVERTERS=ON -DEXCLUDE_ELUDE=OFF $percolatorSource
 
 make -j 8 win32installer
