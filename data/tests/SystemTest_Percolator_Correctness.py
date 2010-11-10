b682f75afea06f8968bf1256277b17f848ee8b68
Mattia Tomasoni <tomasoni@sbc.su.se>
2010-11-10 17:14:09 +0100

    .

--- data/tests/SystemTest_Percolator_Correctness.py
+++ data/tests/SystemTest_Percolator_Correctness.py
@@ -23,7 +23,7 @@ if exitStatus is not None:
   print "...TEST FAILED: percolator (no options) terminated with " + str(exitStatus) + " exit status"
   exit(1)
 
-processFile = os.popen("xmllint --noout --schema http://github.com/percolator/percolator/raw/xml-1-1/src/xml/percolator_out.xsd " +
+processFile = os.popen("xmllint --noout --schema http://per-colator.com/xml/xml-1-1/percolator_out.xsd " +
   os.path.join(path, "data/percolator_test/sqt2pin/pout.xml"))
 exitStatus = processFile.close()
 if exitStatus is not None:
