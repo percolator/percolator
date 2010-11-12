9c4d83992e0cc0a6bc2c3da8f0c53809fc8a19a5
Mattia Tomasoni <tomasoni@sbc.su.se>
2010-11-12 11:09:13 +0100

    added schema location for win32 installer

--- install/ubuntu_percolator_converters.sh
+++ install/ubuntu_percolator_converters.sh
@@ -4,7 +4,6 @@ set -e
 
 base=/tmp
 percolatorInstall=$base/bin
-schemaLocation=$base/share/percolator/xml-1-1
 percolatorBuild=$base/percolatorBuild
 percolatorSource=`/bin/pwd`
 
@@ -38,7 +37,7 @@ fi
 mkdir $percolatorInstall
 cd $percolatorBuild
 
-cmake -G"Eclipse CDT4 - Unix Makefiles" -DSTATIC=off -DCMAKE_BUILD_TYPE=Debug -DGOOGLE_TEST=TRUE -DEXCLUDE_CONVERTERS=FALSE -DEXCLUDE_ELUDE=FALSE -DCMAKE_INSTALL_PREFIX=$percolatorInstall -DCMAKE_PREFIX_PATH=$base/xsd-3.3.0-x86_64-linux-gnu/ -DGOOGLE_TEST_PATH=$base/gtest-1.5.0 -DSCHEMA_LOCATION=$schemaLocation $percolatorSource
+cmake -G"Eclipse CDT4 - Unix Makefiles" -DSTATIC=off -DCMAKE_BUILD_TYPE=Debug -DGOOGLE_TEST=TRUE -DEXCLUDE_CONVERTERS=FALSE -DEXCLUDE_ELUDE=FALSE -DCMAKE_INSTALL_PREFIX=$percolatorInstall -DCMAKE_PREFIX_PATH=$base/xsd-3.3.0-x86_64-linux-gnu/ -DGOOGLE_TEST_PATH=$base/gtest-1.5.0 $percolatorSource
 
 make -j 8
 make install
