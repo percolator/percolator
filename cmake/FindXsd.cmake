0cb781cc407ee8f8b7ea90f17a6242a7a9faf0a5
Lukas Käll <lukas.kall@cbr.su.se>
2011-01-26 17:41:49 +0100

    changed some xsd module pathes

--- cmake/FindXsd.cmake
+++ cmake/FindXsd.cmake
@@ -1,11 +1,11 @@
 FILE(GLOB my_local "$ENV{HOME}/src/xsd*")
 FILE(GLOB my_tmp "/tmp/xsd*")
 
-FIND_PATH(XSD_INCLUDE_DIR xsd/cxx/parser/elements.hxx PATH_SUFFIXES libxsd  PATHS
+FIND_PATH(XSD_INCLUDE_DIR xsd/cxx/parser/elements.hxx PATH_SUFFIXES libxsd PATHS
   "[HKEY_CURRENT_USER\\software\\xsd\\include]"
   "[HKEY_CURRENT_USER]\\xsd\\include]"
-  ${my_local}/include
-  ${my_tmp}/include
+  ${my_tmp}
+  ${my_local}
   /usr/local/include/xsd
   /usr/include/xsd
 )
@@ -14,12 +14,16 @@ FIND_PROGRAM(XSD_EXECUTABLE
   NAMES 
     xsd xsdcxx
   PATHS
-    "[HKEY_CURRENT_USER\\xsd\\bin"
+    "[HKEY_CURRENT_USER\\xsd\\bin]"
     ${my_local}/bin
+    ${my_local}
     ${my_tmp}/bin
     /usr/local/bin
     /usr/bin
 )
+
+#MESSAGE("my_local=${my_local}")
+
 
 # if the include and the program are found then we have it
 IF(XSD_INCLUDE_DIR)
@@ -27,6 +31,9 @@ IF(XSD_INCLUDE_DIR)
     SET( XSD_FOUND "YES" )
   ENDIF(XSD_EXECUTABLE)
 ENDIF(XSD_INCLUDE_DIR)
+
+#MESSAGE("XSD_INCLUDE_DIR=${XSD_INCLUDE_DIR}")
+#MESSAGE("XSD_EXECUTABLE=${XSD_EXECUTABLE}")
 
 MARK_AS_ADVANCED(
   XSD_INCLUDE_DIR
