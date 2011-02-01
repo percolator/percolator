e3b7486cf5b12af11c7b88c6f28c705ab7e5f8fc
Lukas Käll <lukas.kall@cbr.su.se>
2011-02-01 17:50:53 +0100

    edited FindXsd

--- cmake/FindXsd.cmake
+++ cmake/FindXsd.cmake
@@ -4,10 +4,9 @@ FILE(GLOB my_tmp "/tmp/xsd*")
 FIND_PATH(XSD_INCLUDE_DIR xsd/cxx/parser/elements.hxx PATH_SUFFIXES libxsd PATHS
   "[HKEY_CURRENT_USER\\software\\xsd\\include]"
   "[HKEY_CURRENT_USER]\\xsd\\include]"
-  ${my_tmp}
-  ${my_local}
+#  ${my_tmp}/libxsd
+#  ${my_local}/libxsd
   /usr/local/include/xsd
-  /usr/include/xsd
 )
 
 FIND_PROGRAM(XSD_EXECUTABLE 
@@ -15,9 +14,9 @@ FIND_PROGRAM(XSD_EXECUTABLE
     xsd xsdcxx
   PATHS
     "[HKEY_CURRENT_USER\\xsd\\bin]"
-    ${my_local}/bin
-    ${my_local}
-    ${my_tmp}/bin
+#    ${my_local}/bin
+#    ${my_local}
+#    ${my_tmp}/bin
     /usr/local/bin
     /usr/bin
 )
@@ -32,8 +31,8 @@ IF(XSD_INCLUDE_DIR)
   ENDIF(XSD_EXECUTABLE)
 ENDIF(XSD_INCLUDE_DIR)
 
-#MESSAGE("XSD_INCLUDE_DIR=${XSD_INCLUDE_DIR}")
-#MESSAGE("XSD_EXECUTABLE=${XSD_EXECUTABLE}")
+MESSAGE(STATUS "XSD_INCLUDE_DIR=${XSD_INCLUDE_DIR}")
+MESSAGE(STATUS "XSD_EXECUTABLE=${XSD_EXECUTABLE}")
 
 MARK_AS_ADVANCED(
   XSD_INCLUDE_DIR
