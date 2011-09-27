8c3aea0ad3f9cbb310fac18901307f0a5caa6006
Jose Fernandez Navarro <jc.fernandez.navarro@gmail.com>
2011-09-27 16:52:59 +0200

    cross compilation

--- cmake/FindXsd.cmake
+++ cmake/FindXsd.cmake
@@ -1,21 +1,12 @@
-FILE(GLOB my_local "$ENV{HOME}/src/xsd*")
-FILE(GLOB my_tmp "/tmp/xsd*")
-FILE(GLOB my_src "${PROJECT_SOURCE_DIR}/../../libs/xsd*")
-FILE(GLOB my_src2 "${PROJECT_SOURCE_DIR}/../libs/xsd*")
-FILE(GLOB my_src3 "${PROJECT_SOURCE_DIR}/libs/xsd*")
+
 
 FIND_PATH(XSD_INCLUDE_DIR xsd/cxx/parser/elements.hxx PATH_SUFFIXES libxsd PATHS
   "[HKEY_CURRENT_USER\\software\\xsd\\include]"
   "[HKEY_CURRENT_USER]\\xsd\\include]"
-  ${my_tmp}/libxsd
-  ${my_local}/libxsd
-  ${my_src}/libxsd
-  ${my_src2}/libxsd
-  ${my_src3}/libxsd
+  /usr/local/include
   /usr/local/include/xsd
   /usr/include
   /usr/include/xsd
-  /usr/local/include
   "C:/Program Files/CodeSynthesis XSD 3.3/include"
   "D:/Program Files/CodeSynthesis XSD 3.3/include"
 )
@@ -30,13 +21,6 @@ FIND_PROGRAM(XSD_EXECUTABLE
     xsd xsdcxx
   PATHS
     "[HKEY_CURRENT_USER\\xsd\\bin]"
-    ${my_local}/bin
-    ${my_local}
-    ${my_tmp}/bin
-    ${my_src}/bin
-    ${my_src2}/bin
-    ${my_src3}/bin
-    ${my_src3}
     /usr/local/bin
     /usr/bin
     "[HKEY_CURRENT_USER\\xsd\\bin]" $ENV{XSDDIR}/bin
--- cmake/windows_percolator/Toolchain-mingw32.cmake
+++ cmake/windows_percolator/Toolchain-mingw32.cmake
@@ -1,22 +1,21 @@
 SET(CMAKE_SYSTEM_NAME Windows)
 
 # specify the cross compiler
-# SET(CMAKE_C_COMPILER /usr/bin/i686-pc-mingw32-gcc)
-# SET(CMAKE_CXX_COMPILER /usr/bin/i686-pc-mingw32-g++)
 find_file(MINGWV NAMES i586-mingw32msvc-gcc i586-mingw32msvc-g++ PATHS
 			/usr/bin
 			/usr/local/bin)
-if(MINGWV-NOTFOUND)
+if(NOT MINGWV)
   set(MINGW_PREFIX "/usr/bin/i686-pc-mingw32-")
-else(MINGWV-NOTFOUND)
+else(NOT MINGWV)
   set(MINGW_PREFIX "/usr/bin/i586-mingw32msvc-")
-endif()
-set(CMAKE_RC_COMPILER ${MINGW_PREFIX}windres)
+endif(NOT MINGWV)
+
 set(CMAKE_AR ${MINGW_PREFIX}ar)
 set(CMAKE_RANLIB ${MINGW_PREFIX}ranlib)
 set(CMAKE_LINKER ${MINGW_PREFIX}ld)
 set(CMAKE_C_COMPILER ${MINGW_PREFIX}gcc)
 set(CMAKE_CXX_COMPILER ${MINGW_PREFIX}g++)
+set(CMAKE_RC_COMPILER ${MINGW_PREFIX}windres)
 set(CMAKE_EXECUTABLE_SUFFIX ".exe")
 
 # Boost Configuration
@@ -24,9 +23,13 @@ SET(Boost_COMPILER "-gcc45")
 SET(Boost_USE_STATIC_LIBS ON) 
 # where is the target environment
 SET(CMAKE_FIND_ROOT_PATH /usr/i686-pc-mingw32;/usr/i586-mingw32msvc;/usr/i686-pc-mingw32/sys-root/mingw;/usr/i586-mingw32msvc/sys-root/mingw;
+			  /home/USERNAME/i586-mingw32msvc;/home/USERNAME/i686-pc-mingw32;
 			  ${PROJECT_SOURCE_DIR}/../libs;${PROJECT_SOURCE_DIR}/libs;${PROJECT_SOURCE_DIR}/../../libs;
-			  ${PROJECT_SOURCE_DIR}/libs/xsd-3.0.0-i686-windows/libxsd;${PROJECT_SOURCE_DIR}/../libs/xsd-3.0.0-i686-windows/libxsd
-			  ${PROJECT_SOURCE_DIR}/../../libs/xsd-3.0.0-i686-windows/libxsd)
+			  ${PROJECT_SOURCE_DIR}/libs/xsd-3.3.0-i686-windows/libxsd;${PROJECT_SOURCE_DIR}/../libs/xsd-3.3.0-i686-windows/libxsd
+			  ${PROJECT_SOURCE_DIR}/../../libs/xsd-3.3.0-i686-windows/libxsd
+			  ${PROJECT_SOURCE_DIR}/libs/xsd-3.3.0-i686-windows;${PROJECT_SOURCE_DIR}/../libs/xsd-3.3.0-i686-windows
+			  ${PROJECT_SOURCE_DIR}/../../libs/xsd-3.3.0-i686-windows)
+
 # search for programs in the build host directories
 SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 # for libraries and headers in the target directories
