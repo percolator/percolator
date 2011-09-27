10e64984c99b7a86e29f8712eabd72197c788ff4
Jose Fernandez Navarro <jc.fernandez.navarro@gmail.com>
2011-09-27 16:07:18 +0200

    cross compiler

--- cmake/windows_percolator/Toolchain-mingw32.cmake
+++ cmake/windows_percolator/Toolchain-mingw32.cmake
@@ -18,7 +18,10 @@ set(CMAKE_LINKER ${MINGW_PREFIX}ld)
 set(CMAKE_C_COMPILER ${MINGW_PREFIX}gcc)
 set(CMAKE_CXX_COMPILER ${MINGW_PREFIX}g++)
 set(CMAKE_EXECUTABLE_SUFFIX ".exe")
-set (Boost_COMPILER "-gcc44")
+
+# Boost Configuration
+SET(Boost_COMPILER "-gcc45")
+SET(Boost_USE_STATIC_LIBS ON) 
 # where is the target environment
 SET(CMAKE_FIND_ROOT_PATH /usr/i686-pc-mingw32;/usr/i586-mingw32msvc;/usr/i686-pc-mingw32/sys-root/mingw;/usr/i586-mingw32msvc/sys-root/mingw;
 			  ${PROJECT_SOURCE_DIR}/../libs;${PROJECT_SOURCE_DIR}/libs;${PROJECT_SOURCE_DIR}/../../libs;
@@ -30,33 +33,4 @@ SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 
-# FindQt4.cmake queries qmake to get information,
-# which doesn't work when crosscompiling
-# SET(QT_HEADERS_DIR ${CMAKE_FIND_ROOT_PATH}/include)
-# SET(QT_LIBRARY_DIR ${CMAKE_FIND_ROOT_PATH}/lib)
 
-# #----------------------------------------------------------
-# # disable dynamic links
-# #----------------------------------------------------------
-# SET(CMAKE_SKIP_RPATH TRUE)
-# SET(CMAKE_SHARED_LIBRARY_C_FLAGS "")            # -pic
-# SET(CMAKE_SHARED_LIBRARY_CREATE_C_FLAGS "")       # -shared
-# SET(CMAKE_SHARED_LIBRARY_LINK_C_FLAGS "")         # +s, flag for exe link to use shared lib
-# SET(CMAKE_SHARED_LIBRARY_RUNTIME_C_FLAG "")       # -rpath
-# SET(CMAKE_SHARED_LIBRARY_RUNTIME_C_FLAG_SEP "")   # : or empty
-# 
-# SET(CMAKE_LINK_LIBRARY_SUFFIX "")
-# SET(CMAKE_STATIC_LIBRARY_PREFIX "lib")
-# SET(CMAKE_STATIC_LIBRARY_SUFFIX ".a")
-# SET(CMAKE_SHARED_LIBRARY_PREFIX "lib")          # lib
-# SET(CMAKE_SHARED_LIBRARY_SUFFIX ".a")           # .a
-# SET(CMAKE_EXECUTABLE_SUFFIX "")          # .exe
-# SET(CMAKE_DL_LIBS "" )
-# 
-# SET(CMAKE_FIND_LIBRARY_PREFIXES "lib")
-# SET(CMAKE_FIND_LIBRARY_SUFFIXES ".a")
-# 
-# SET(CMAKE_CXX_LINK_SHARED_LIBRARY)
-# SET(CMAKE_CXX_LINK_MODULE_LIBRARY)
-# SET(CMAKE_C_LINK_SHARED_LIBRARY)
-# SET(CMAKE_C_LINK_MODULE_LIBRARY)
