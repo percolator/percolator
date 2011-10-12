e8bf29a811246fa4695424bea94b5d5d49f41484
Jose Fernandez Navarro <jc.fernandez.navarro@gmail.com>
2011-10-12 14:11:56 +0200

    xercesc fixed windows

--- cmake/FindXercesC.cmake
+++ cmake/FindXercesC.cmake
@@ -10,8 +10,15 @@ FIND_PATH(XERCESC_INCLUDE_DIR xercesc/dom/DOM.hpp xercesc/parsers/SAXParser.hpp
    HINTS
    "[HKEY_CURRENT_USER\\software\\xerces-c\\src]"
    "[HKEY_CURRENT_USER\\xerces-c\\src]"
-   $ENV{XERCESCROOT}/src/
-   $ENV{XERCESCROOT}/include/
+   $ENV{XERCESCROOT}/src
+   $ENV{XERCESCROOT}/include
+   $ENV{CODESYNTH}/include
+   $ENV{XSDDIR}/include
+   ${XERCES_DIR}/include
+   $ENV{XERCES_DIR}/include
+   ${XERCESCROOT}/include
+   ${CODESYNTH}/include
+   ${XSDDIR}/include
    /usr/include
    /usr/local/include
    PATH_SUFFIXES xerces-c include
@@ -22,10 +29,32 @@ FIND_LIBRARY(XERCESC_LIBRARIES NAMES xerces-c xerces-c_3 xerces-c_2 xerces-c_sta
    "[HKEY_CURRENT_USER\\software\\xerces-c\\lib]"
    "[HKEY_CURRENT_USER\\xerces-c\\lib]"
    $ENV{XERCESCROOT}/lib
+   $ENV{CODESYNTH}/lib
+   $ENV{XSDDIR}/lib
+   ${XERCES_DIR}/lib
+   $ENV{XERCES_DIR}/lib
+   ${XERCESCROOT}/lib
+   ${CODESYNTH}/lib
+   ${XSDDIR}/lib
    /usr/lib
    /usr/local/lib
    PATH_SUFFIXES lib64 lib32 lib
    )
+
+if( MSVC )
+# make sure we use the right compiler version
+
+	if( MSVC80 )
+		set( XERCESC_COMPILER_PREFIX vc-8.0 )
+	elseif( MSVC90 )
+		set( XERCESC_COMPILER_PREFIX vc-9.0 )
+	elseif( MSVC10 )
+		set( XERCESC_COMPILER_PREFIX vc-10.0 )
+	else( MSVC10 )
+		message( FATAL_ERROR "Unknown MSVC version!" )
+	endif( MSVC80 ) 
+		
+endif( MSVC )
 
 # if the include a the library are found then we have it
 SET( XERCESC_FOUND 0 )
