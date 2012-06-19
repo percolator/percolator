01a36b161668e3cedf25187c5027ae63a47864b1
Jose Fernandez <jc.fernandez.navarro@gmail.com>
2012-06-19 14:51:36 +0200

    I modified the structure of sqt2pin to make it more object oriented, I also added some new executables (tandem2pin...) and I created a base class for the common parsing functions

---
+++ src/converters/tandem2pin.h
@@ -0,0 +1,30 @@
+/*
+    Copyright 2012 <copyright holder> <email>
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+
+#ifndef TANDEM2PIN_H
+#define TANDEM2PIN_H
+
+class tandem2pin
+{
+
+public:
+tandem2pin();
+tandem2pin(const tandem2pin& other);
+virtual ~tandem2pin();
+};
+
+#endif // TANDEM2PIN_H
