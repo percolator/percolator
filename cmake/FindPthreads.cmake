8d9d77ab011aad4b462c77db462b23cd48ea7739
Jose Fernandez <jc.fernandez.navarro@gmail.com>
2012-11-13 13:23:16 +0100

    fixed some erros in the CMake files

--- cmake/FindPthreads.cmake
+++ cmake/FindPthreads.cmake
@@ -62,7 +62,7 @@ FIND_PATH(PTHREADS_INCLUDE_DIR
 	  $ENV{PTHREAD_INCLUDE_PATH}
 	  )
 
-message(STATUS "${PTHREADS_INCLUDE_DIR}")
+#message(STATUS "${PTHREADS_INCLUDE_DIR}")
 #
 # Find the library
 #
@@ -100,7 +100,7 @@ FIND_LIBRARY(PTHREADS_LIBRARY NAMES ${names}
 	/usr/i686-pc-mingw32/lib/
 	C:/MinGW/lib/
 	/mingw/lib  )
-message(STATUS "${PTHREADS_LIBRARY}")
+#message(STATUS "${PTHREADS_LIBRARY}")
 
 INCLUDE(FindPackageHandleStandardArgs)
 FIND_PACKAGE_HANDLE_STANDARD_ARGS(Pthreads DEFAULT_MSG
