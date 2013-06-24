ce61aeaff350ccb7d2b4dd35045a856aaedbaa3a
Ali Narangifard <ali.narangifard@scilifelab.se>
2013-06-24 20:00:50 +0200

    modifications on /admin/vagrant

--- admin/vagrant/precise64/build_precise.sh
+++ admin/vagrant/precise64/build_precise.sh
@@ -6,7 +6,7 @@ release="$HOME/release";
 #---------------------------------------------------------------------------------------
 builder="precise64_build.sh";
 builder_adr="../../builders/"
-fedora_box_url="http://files.vagrantup.com/precise64.box";
+precise_box_url="http://files.vagrantup.com/precise64.box";
 #---------------------------------------------------------------------------------------
 #managing source and release directories
 if [ $1 ]
@@ -35,7 +35,7 @@ cat <<EOF > Vagrantfile
 Vagrant.configure("2") do |config|
   config.vm.box = "precise64"
   config.vm.box_url = "${precise_box_url}"
-  config.vm.provision :shell, :inline => "sudo -u vagrant bash /vagrant/${builder} /vagrant/src /vagrant/build${post}"
+  config.vm.provision :shell, :inline => "export HOME=/home/vagrant;sudo -u vagrant bash /vagrant/${builder} /vagrant/src /vagrant/build${post}"
 end
 EOF
 #-----------------end of Vagrantfile content--------
@@ -45,7 +45,9 @@ vagrant up
 
 #---------------------------------------------------------------------------------------
 # release:
+cp ${tmp_dir}/build${post}/percolator/per*.rpm ${release};
 cp ${tmp_dir}/build${post}/percolator/per*.deb ${release};
+cp ${tmp_dir}/build${post}/converters/per*.rpm ${release};
 cp ${tmp_dir}/build${post}/converters/per*.deb ${release};
 
 #---------------------------------------------------------------------------------------
