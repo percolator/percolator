6a94eb3155e8a64def46328fbbf13d35a38a61d3
Ali Narangifard <ali.narangifard@scilifelab.se>
2013-07-11 16:27:10 +0200

    current path insensitive

--- admin/vagrant/build_all_platforms.sh
+++ admin/vagrant/build_all_platforms.sh
@@ -6,13 +6,15 @@ tmp_dir="$(mktemp -d --tmpdir master_tmp_XXXX)";
 mkdir ${tmp_dir}/src;
 mkdir -p ${release};
 #-------------------------------------------------------------
-Spath=${BASH_SOURCE%build_all*};
+Ppath="$(dirname ${BASH_SOURCE})";
+cd ${Ppath};
+Spath=$PWD;
 #-------------------------------------------------------------
 # clone :
 git clone --branch ${branch} https://github.com/percolator/percolator.git ${tmp_dir}/src/percolator;
 #-------------------------------------------------------------
-cd ${Spath}precise64;bash build_precise.sh ${tmp_dir} ${release}/precise;
-cd ${Spath}fedora;bash build_fedora.sh ${tmp_dir} ${release}/fedora;
-cd ${Spath}mingw;bash build_win.sh ${tmp_dir} ${release}/mingw;
+cd ${Spath}/precise64;bash build_precise.sh ${tmp_dir} ${release}/precise;
+cd ${Spath}/fedora;bash build_fedora.sh ${tmp_dir} ${release}/fedora;
+cd ${Spath}/mingw;bash build_win.sh ${tmp_dir} ${release}/mingw;
 #-------------------------------------------------------------
 
