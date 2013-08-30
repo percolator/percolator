6cb961b01bbdfa7591fffdf1e32aab6c90df421b
Ali Narangifard <ali.narangifard@scilifelab.se>
2013-08-30 12:33:44 +0200

    builder on OS X

---
+++ admin/builders/osx_build.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+current_path=$PWD
+Ppath="$(dirname ${BASH_SOURCE})"
+cd ${Ppath}
+Spath=$PWD
+
+if [ $1 ]
+        then
+        release=$1
+else 
+        release=$HOME/release
+fi 
+
+tmp_dir="$(mktemp -d /tmp/OSX_XXXX)"
+mkdir ${tmp_dir}/src ${tmp_dir}/build ${tmp_dir}/src/percolator
+src="${tmp_dir}/src";build="${tmp_dir}/build";
+
+cp -R "${Spath}"/../../ ${src}/percolator/
+
+sudo port install tokyocabinet boost zlib
+
+#----------------------------------------
+xsd=xsd-3.3.0-i686-macosx
+cd ${src}
+curl -O http://www.codesynthesis.com/download/xsd/3.3/macosx/i686/${xsd}.tar.bz2
+tar -xjf ${xsd}.tar.bz2
+sed -i -e 's/setg/this->setg/g' ${xsd}/libxsd/xsd/cxx/zc-istream.txx
+sed -i -e 's/ push_back/ this->push_back/g' ${xsd}/libxsd/xsd/cxx/tree/parsing.txx
+sed -i -e 's/ push_back/ this->push_back/g' ${xsd}/libxsd/xsd/cxx/tree/stream-extraction.hxx
+
+extr=${xsd}/libxsd/xsd/cxx/tree/xdr-stream-extraction.hxx
+inse=${xsd}/libxsd/xsd/cxx/tree/xdr-stream-insertion.hxx
+sed -i -e 's/ uint8_t/ unsigned char/g' ${extr} ${inse}
+sed -i -e 's/ int8_t/ char/g' ${extr} ${inse}
+sed -i -e 's/xdr_int8_t/xdr_char/g' ${extr} ${inse}
+sed -i -e 's/xdr_uint8_t/xdr_u_char/g' ${extr} ${inse}
+#------------------------------------------
+xer=xerces-c-3.1.1
+
+#curl -O http://apache.mirrors.spacedump.net//xerces/c/3/sources/${xer}.tar.gz
+
+#mkdir -p ${build}
+#cd ${build}
+#tar xzf ${src}/${xer}.tar.gz 
+#cd ${xer}/
+#./configure CFLAGS="-arch x86_64" CXXFLAGS="-arch x86_64" 
+#cd src/
+#make
+#sudo make install
+
+#-------------------------------------------
+mkdir -p ${build}/percolator
+cd ${build}/percolator
+
+cmake -DTARGET_ARCH=amd64 -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_PREFIX_PATH="${src}/${xsd}/"  ${src}/percolator
+make package
+
+mkdir -p ${build}/converters
+cd ${build}/converters
+
+cmake -DTARGET_ARCH=amd64 -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_PREFIX_PATH="${src}/${xsd}/"  ${src}/percolator/src/converters
+make package
+#--------------------------------------------
+mkdir -p ${release}
+cp ${build}/percolator/*.dmg ${release}
+cp ${build}/converters/*.dmg ${release}
