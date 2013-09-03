3590cc93c96bdf201bb5beb278cdc9a8fa0d4294
Ali Narangifard <ali.narangifard@scilifelab.se>
2013-09-03 15:56:42 +0200

    qvality's return value fixed.

--- src/qvality/qvality_main.cpp
+++ src/qvality/qvality_main.cpp
@@ -24,7 +24,7 @@ int main(int argc, char** argv) {
     try
     {
      if (pCaller->parseOptions(argc, argv)) {
-       retVal=pCaller->run();
+     	if (pCaller->run()) retVal=0;
      }
     } 
     catch (const std::exception& e) 
