ed94f166a077e475ff7288dadfe2378eb5e47d5a
Lukas Käll <lukas.kall@gmail.com>
2013-12-17 22:19:15 +0100

    moving over the system tests to master

--- data/system_tests/percolator/SystemTest_Qvality_Correctness.py.cmake
+++ data/system_tests/percolator/SystemTest_Qvality_Correctness.py.cmake
@@ -7,6 +7,7 @@ import sys
 
 pathToBinaries = "@pathToBinaries@"
 pathToData = "@pathToData@"
+tmpDir = "@pathToData@"
 success = True
 
 print "QVALITY CORRECTNESS"
@@ -16,12 +17,12 @@ print "(*): running qvality..."
 processFile = os.popen("(" + os.path.join(pathToBinaries, "qvality ") + 
   os.path.join(pathToData, "qvality/target.xcorr ") + 
   os.path.join(pathToData, "qvality/null.xcorr ") + 
-  "2>&1) > /tmp/qvalityOutput.txt")
+  "2>&1) > " + tmpDir + "/qvalityOutput.txt")
 
 exitStatus = processFile.close()
 if exitStatus is not None:
   print "...TEST FAILED: qvality terminated with " + str(exitStatus) + " exit status"
-  print "check /tmp/qvalityOutput.txt for details" 
+  print "check qvalityOutput.txt for details" 
   success = False
 
 # if no errors were encountered, succeed
