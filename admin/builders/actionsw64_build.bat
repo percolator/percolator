3fdc11caf8d7baf351617511cee1fdd24c1b6ca8
Lukas Kall <lukas.kall@gmail.com>
2020-04-21 08:20:36 +0200

    Almost good

--- admin/builders/actionsw64_build.bat
+++ admin/builders/actionsw64_build.bat
@@ -174,11 +174,10 @@ if not exist "%BUILD_DIR%" (md "%BUILD_DIR%")
 if not exist "%BUILD_DIR%\percolator-noxml" (md "%BUILD_DIR%\percolator-noxml")
 cd /D "%BUILD_DIR%\percolator-noxml"
 echo cmake percolator-noxml.....
-%CMAKE_EXE% -G "Visual Studio 16 2019" -A x64 -DCMAKE_BUILD_TYPE=%BUILD_TYPE% -DXML_SUPPORT=OFF "%SRC_DIR%\percolator"
+cmake.exe -G "Visual Studio 16 2019" -A x64 -DCMAKE_BUILD_TYPE=%BUILD_TYPE% -DXML_SUPPORT=OFF "%SRC_DIR%\percolator"
 echo build percolator (this will take a few minutes).....
 dir
-echo "msbuild PACKAGE.vcxproj /p:VCTargetsPath=%VCTARGET% /p:Configuration=%BUILD_TYPE% /m"
-msbuild PACKAGE.vcxproj /p:VCTargetsPath=%VCTARGET% /p:Configuration=%BUILD_TYPE% /m
+msbuild PACKAGE.vcxproj /p:Configuration=%BUILD_TYPE% /m
 
 ::msbuild INSTALL.vcxproj /p:VCTargetsPath="%VCTARGET%" /p:Configuration=%BUILD_TYPE% /m
 ::msbuild RUN_TESTS.vcxproj /p:VCTargetsPath="%VCTARGET%" /p:Configuration=%BUILD_TYPE% /m
