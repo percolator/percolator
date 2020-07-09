fb0f0291582e9eb603d7523aaa74b32332293409
pat <pk115166@umconnect.umt.edu>
2020-07-09 14:52:15 -0600

    removed old include statement

--- src/CrossValidation.cpp
+++ src/CrossValidation.cpp
@@ -1,4 +1,4 @@
- /*******************************************************************************
+ /******************************************************************************
  Copyright 2006-2012 Lukas Käll <lukas.kall@scilifelab.se>
 
  Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,7 +13,7 @@
  See the License for the specific language governing permissions and
  limitations under the License.
 
- *******************************************************************************/
+ ******************************************************************************/
 
 #include "CrossValidation.h"
 
