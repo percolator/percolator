4e6f55c613b4afdb047e151fd0f4e9a8a52a545a
marcus1337 <marcus1337@github.com>
2020-10-29 11:53:27 +0100

    relative differences graph

--- data/integration_tests/percolator/IntegrationTest_Percolator_SpeedGraph.py
+++ data/integration_tests/percolator/IntegrationTest_Percolator_SpeedGraph.py
@@ -17,7 +17,10 @@ import ast
 from matplotlib.cm import get_cmap
 import matplotlib.lines as mlines
 from matplotlib.ticker import FormatStrFormatter
+import matplotlib.ticker as mtick
+import math
 
+import numpy as np
 
 markers = ["o","v","^","<",">","1","s","p","P","*","H","x","d","|","+"]
 outputFilename = "outGraph"
@@ -56,12 +59,12 @@ def getPlotColors():
     cmap = get_cmap(colorScheme)  # type: matplotlib.colors.ListedColormap
     return cmap.colors 
 
-def setBarPlotLegends(data, colors):
-    colorCounter = 0
+def setBarPlotLegends(data, colors, counterStart = 0):
+    counter = counterStart
     legendColors = {}
     for keyName in data.keys():
-        legendColors[keyName] = colors[colorCounter]
-        colorCounter += 1      
+        legendColors[keyName] = colors[counter]
+        counter += 1      
     labels = list(legendColors.keys())
     handles = [plt.Rectangle((0,0),1,1, color=legendColors[label]) for label in labels]
     plt.legend(handles, labels)
@@ -82,22 +85,20 @@ def setDescriptiveText(minExponent, yTitleName):
     plt.xlabel('Number of PSMs x$10^{}$'.format(len(str(minExponent))-1), fontsize='x-large')
     plt.ylabel(yTitleName, fontsize='x-large')
 
-def getBarChart(data):
+def prepareBarChart(data):
     colors = getPlotColors()
     df = pd.DataFrame(data)
 
     ax = df.plot(kind='bar', width=0.4, color=colors, zorder=3)
-    ax.grid(zorder=0)
+    ax.grid(zorder=0, axis='y')
 
     setBarPlotLegends(data, colors)
     plt.setp(plt.xticks()[1], rotation=0)
-    return plt
 
 def renderLines(data, colors):
     dataFrames = []
     for keyName in data.keys():
         tmpData = {keyName: data[keyName]}
-        print(tmpData)
         dataFrames.append(pd.DataFrame(tmpData))
     prevAx = None
     counter = 0
@@ -114,7 +115,7 @@ def formatHorizontalLine(data):
     numXBins = len(data[next(iter(data))])
     plt.locator_params(axis = 'x', nbins = numXBins)
 
-def getLineChart(data):
+def prepareLineChart(data):
     colors = getPlotColors()
     renderLines(data, colors)
 
@@ -141,31 +142,100 @@ def getXValues(file):
     xSteps = [int(item/minExponent) for item in xSteps]
     return xSteps, minExponent
 
-def makeBarChart(data, minExponent, yTitleName, outputFilename):
-    chart = getBarChart(data)
-    setDescriptiveText(minExponent, yTitleName)
+def savePlot(name):
     plt.tight_layout()
-    chart.savefig("{}/{}.png".format(pathToOutputData, outputFilename + "Bar"))
-    chart.close()
+    plt.savefig("{}/{}.png".format(pathToOutputData, name))
+    plt.close()
+
+def makeBarChart(data, minExponent, yTitleName, outputFilename):
+    prepareBarChart(data)
+    setDescriptiveText(minExponent, yTitleName)
+    savePlot(outputFilename + "Bar")
 
 def makeLineChart(data, minExponent, yTitleName, outputFilename):
-    chart = getLineChart(data)
+    prepareLineChart(data)
     setDescriptiveText(minExponent, yTitleName)
-    plt.tight_layout()
-    chart.savefig("{}/{}.png".format(pathToOutputData, outputFilename + "Line"))
-    chart.close()
+    savePlot(outputFilename + "Line")
 
+def getMaxAndMinPercentage(data):
+    minVal = 999999999999
+    maxVal = -99999999999
+    for value in data.values():
+        percentages = list(value.values())
+        maxVal = max(maxVal, math.ceil(max(percentages))) #max av all kommande data
+        minVal = min(minVal, min(percentages))
+    return minVal, maxVal
 
+def setRelativePercentages(data, referenceValues):
+    for key in data.keys():
+        relativeValues = data[key].values()
+        percentages = [y/x for x, y in zip(referenceValues, relativeValues)] #Assume it's impossible for any run-time to be 0.
+        percentages = [ round(elem, 3) for elem in percentages ]
+        data[key].update(zip(data[key], percentages))
+
+def addRelativeLines(data, xTicks, colors):
+    counter = 1
+    for key in data.keys():
+        #print(data[key].values())
+        plt.semilogy(xTicks, data[key].values(), color=colors[counter], marker=markers[counter], base=2)
+        counter += 1
+
+def extractReferenceValuesAndXTicks(tmpData):
+    dataIter = iter(tmpData) 
+    firstKey = next(dataIter)
+    referenceValues = tmpData[firstKey].values()
+    xTicks = list(tmpData[firstKey].keys())
+    del tmpData[firstKey]
+    return referenceValues, xTicks
+
+def nextClosePowerOf2(x):  
+    if x >= 1:
+        x = int(x)
+        return 1 if x == 0 else 2**(x - 1).bit_length()
+    x = int(x*1000.0)
+    x = 2**(x - 1).bit_length()
+    x /= 1000.0
+    return x
+
+def makeRelativeGraph(data, minExponent, yTitleName, outputFilename):
+    colors = getPlotColors()
+    tmpData = data.copy() #Assume first file is used as a "reference" for other files. I.e. how speed changes relative to the first file.
+    referenceValues, xTicks = extractReferenceValuesAndXTicks(tmpData)
+    setRelativePercentages(tmpData, referenceValues)
+
+    min_value, max_value = getMaxAndMinPercentage(tmpData)
+    min_value = nextClosePowerOf2(min_value)/2
+    max_value = nextClosePowerOf2(max_value)
+
+    ax = plt.gca()
+    ax.grid(True, which="both", zorder=0)
+    setBarPlotLegends(tmpData, colors, 1)
+
+    plt.axhline(y=1, color='silver', linestyle='--', linewidth=1)
+    addRelativeLines(tmpData, xTicks, colors)
+    ax.set_ylim(bottom=min_value, top=max_value)
+    setDescriptiveText(minExponent, yTitleName)
+    ax.yaxis.set_major_formatter(mtick.ScalarFormatter())
+    ax.yaxis.get_major_formatter().set_scientific(False)
+    ax.yaxis.get_major_formatter().set_useOffset(False)
+    ax.yaxis.set_major_formatter(FormatStrFormatter('%g'))
+    ax.set_xticks(xTicks)
+    plt.setp(plt.xticks()[1], rotation=0)
+    savePlot(outputFilename + "Relative")
+    
+
+    
 files, fileNames = getFiles()
 xSteps, minExponent = getXValues(files[0]) #Assumes all plots share the same step size for the horizontal line, x.
-print(xSteps)
-print(minExponent)
 dataWall, dataCPU = getPlotData(files, fileNames, xSteps)
-
 
 makeBarChart(dataWall, minExponent,  'Wall Time in Seconds', outputFilename + "Wall")
 makeBarChart(dataCPU, minExponent,   'CPU Time in Seconds', outputFilename + "CPU")
 makeLineChart(dataWall, minExponent, 'Wall Time in Seconds', outputFilename + "Wall")
 makeLineChart(dataCPU, minExponent,  'CPU Time in Seconds', outputFilename + "CPU")
+
+makeRelativeGraph(dataWall, minExponent, "Fraction of Original {} Time".format("Wall"), outputFilename + "Wall")
+makeRelativeGraph(dataCPU, minExponent, "Fraction of Original {} Time".format("CPU"), outputFilename + "CPU")
+
 
 
