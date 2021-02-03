aac6507a4b07be5355fb0c1762ba93143c526b0f
ylu465 <ylu465@uw.edu>
2021-02-03 15:53:01 -0800

    resolve conflicts w.r.t main branch

--- src/CrossValidation.h
+++ src/CrossValidation.h
@@ -45,7 +45,7 @@ class CrossValidation {
   CrossValidation(bool quickValidation, bool reportPerformanceEachIteration, 
     double testFdr, double selectionFdr, double initialSelectionFdr, 
     double selectedCpos, double selectedCneg, int niter, bool usePi0,
-          int nestedXvalBins, bool trainBestPositive, unsigned int numThreads,
+	unsigned int nestedXvalBins, bool trainBestPositive, unsigned int numThreads,
     bool skipNormalizeScores, bool peptideInSameFold);
 
   
