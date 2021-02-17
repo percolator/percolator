da99f339565d15bc36c06c79e6a0de2e46134357
percolator <lukas.kall@scilifelab.se>
2021-02-17 17:12:06 +0100

    Extended xerces compiöation to ubuntu 20.04

--- admin/builders/ubuntu64_build.sh
+++ admin/builders/ubuntu64_build.sh
@@ -69,7 +69,7 @@ else
 fi
 
 # issue with XercesC in Ubuntu 16.04: https://github.com/percolator/percolator/issues/188
-if [[ $(lsb_release -a) == *"16.04"* ]] || [[ $(lsb_release -a) == *"18.04"* ]]; then
+if [[ $(lsb_release -a) == *"16.04"* ]] || [[ $(lsb_release -a) == *"18.04"* ]] || [[ $(lsb_release -a) == *"20.04"* ]]; then
   if [[ ! -d ${ubuntu_xerces}/lib ]]; then
     echo "Installing XercesC"
     # download, compile and link xerces
