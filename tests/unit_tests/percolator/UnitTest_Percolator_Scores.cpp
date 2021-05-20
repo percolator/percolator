b49e1e5f56e5cad4baebcf4ca6895d7e6edff5be
Brian Raiter <breadbox@muppetlabs.com>
2021-05-20 05:01:44 -0700

    Initial tests for Scores.cpp.

---
+++ tests/unit_tests/percolator/UnitTest_Percolator_Scores.cpp
@@ -0,0 +1,166 @@
+/*
+ * Copyright 2021 Brian Raiter <breadbox@muppetlabs.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+/*
+ * Unit tests for the Scores and ScoreHolder classes.
+ */
+
+#include <gtest/gtest.h>
+#include <cstdarg>
+#include "SetHandler.h"
+#include "DataSet.h"
+#include "Scores.h"
+
+// Some strings in alphabetical order.
+static std::string const psmNames[] = { "ABC", "DEF", "GHI", "JKL", "MNO" };
+
+class ScoreHolderTest : public ::testing::Test {
+  protected:
+    bool checkOrder(std::vector<ScoreHolder> const &scores, ...);
+};
+
+// A simple function that verifies that the scores in a vector are in
+// a specific order. (The function is variadic so that the caller
+// doesn't need to declare an extra array.)
+bool ScoreHolderTest::checkOrder(std::vector<ScoreHolder> const &scores, ...)
+{
+    va_list values;
+    va_start(values, scores);
+    for (std::vector<ScoreHolder>::const_iterator it = scores.begin() ;
+             it != scores.end() ; ++it) {
+        double score = va_arg(values, double);
+        if (it->score != score)
+            return false;
+    }
+    va_end(values);
+    return true;
+}
+
+// Verify the ScoreHolder's ordering functions.
+TEST_F(ScoreHolderTest, CheckOrderingFunctions)
+{
+    std::vector<ScoreHolder> scores;
+
+    // peptides are assigned in alphabetic order
+    // scan values are assigned [ 2, 3, 4, 0, 1 ]
+    scores.clear();
+    for (int i = 0 ; i < 5 ; ++i) {
+        PSMDescription *pPSM = new PSMDescription(psmNames[i]);
+        pPSM->scan = (i + 2) % 4;
+        scores.push_back(ScoreHolder(1.0 + i, +1, pPSM));
+    }
+    std::sort(scores.begin(), scores.end(), lexicOrderProb());
+    ASSERT_TRUE(checkOrder(scores, 1.0, 2.0, 3.0, 4.0, 5.0));
+    std::sort(scores.begin(), scores.end(), OrderScanMassCharge());
+    ASSERT_TRUE(checkOrder(scores, 3.0, 4.0, 5.0, 1.0, 2.0));
+
+    // scan values are assigned [ 2, 2, 1, 1, 0 ]
+    // labels are assigned [ -1, +1, -1, +1, -1 ]
+    scores.clear();
+    for (int i = 0 ; i < 5 ; ++i) {
+        PSMDescription *pPSM = new PSMDescription();
+        pPSM->scan = 2 - i / 2;
+        scores.push_back(ScoreHolder(1.0 + i, (i % 2 ? +1 : -1), pPSM));
+    }
+    std::sort(scores.begin(), scores.end(), OrderScanLabel());
+    ASSERT_TRUE(checkOrder(scores, 5.0, 4.0, 3.0, 2.0, 1.0));
+}
+
+// Verify the ScoreHolder's uniqueness functions.
+TEST_F(ScoreHolderTest, CheckUniquenessFilter)
+{
+    std::vector<ScoreHolder> scores;
+
+    // use unique peptides but repeating scan values
+    scores.clear();
+    for (int i = 0 ; i < 5 ; ++i) {
+        PSMDescription *pPSM = new PSMDescription(psmNames[i]);
+        pPSM->scan = i / 2;
+        scores.push_back(ScoreHolder(1.0 + i, +1, pPSM));
+    }
+    ASSERT_EQ(5, scores.size());
+    scores.erase(std::unique(scores.begin(), scores.end(), UniqueScanLabel()),
+                 scores.end());
+    ASSERT_EQ(3, scores.size());
+}
+
+
+class ScoresTest : public ::testing::Test {
+  protected:
+    virtual void SetUp();
+    virtual void TearDown();
+  private:
+    int origVerbose;
+};
+
+void ScoresTest::SetUp()
+{
+    origVerbose = Globals::getInstance()->getVerbose();
+    Globals::getInstance()->setVerbose(0);
+}
+
+void ScoresTest::TearDown()
+{
+    Globals::getInstance()->setVerbose(origVerbose);
+}
+
+// Test the basic populateWithPsms() method.
+TEST_F(ScoresTest, CheckPopulating)
+{
+    Scores scores(true);
+    SetHandler setHandler(0);
+    DataSet *set1 = new DataSet();
+    DataSet *set2 = new DataSet();
+    set1->setLabel(+1);
+    set2->setLabel(-1);
+    for (int i = 0 ; i < 5 ; ++i) {
+        PSMDescription *psm;
+        psm = new PSMDescription(psmNames[i]);
+        psm->scan = 100 + i;
+        set1->registerPsm(psm);
+        psm = new PSMDescription(psmNames[i]);
+        psm->scan = 105 + i;
+        set2->registerPsm(psm);
+    }
+    setHandler.push_back_dataset(set1);
+    setHandler.push_back_dataset(set2);
+
+    scores.populateWithPSMs(setHandler);
+    ASSERT_EQ(10, scores.size());
+
+    unsigned scanValue = 100;
+    for (std::vector<ScoreHolder>::const_iterator it = scores.begin() ;
+            it != scores.end() ;
+            it++, scanValue++) {
+        ASSERT_EQ(scanValue, it->pPSM->scan);
+    }
+}
+
+// Test that populateWithPsms() refuses empty sets.
+TEST_F(ScoresTest, CheckPopulatingEmpty)
+{
+    Scores scores(true);
+    SetHandler setHandler(0);
+    EXPECT_THROW(scores.populateWithPSMs(setHandler), MyException);
+    DataSet *set1 = new DataSet();
+    DataSet *set2 = new DataSet();
+    set1->setLabel(+1);
+    set2->setLabel(-1);
+    setHandler.push_back_dataset(set1);
+    setHandler.push_back_dataset(set2);
+    EXPECT_THROW(scores.populateWithPSMs(setHandler), MyException);
+}
