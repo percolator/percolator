a4d5d6270145df74e8937bf882459d807d2c33d6
Brian Raiter <breadbox@muppetlabs.com>
2021-05-29 22:14:31 -0700

    Adding tests for preIterationSetup and doStep.

---
+++ tests/unit_tests/percolator/UnitTest_Percolator_CrossValidation.cpp
@@ -0,0 +1,153 @@
+/*
+ * Copyright 2020 Brian Raiter <breadbox@muppetlabs.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#include <gtest/gtest.h>
+#include "SetHandler.h"
+#include "CrossValidation.h"
+#include "Globals.h"
+
+/* A subclass of CrossValidation that gives us access to some
+ * protected fields.
+ */
+class CrossValidationEx : public CrossValidation {
+public:
+    CrossValidationEx(bool quickValidation,
+                      bool reportPerformanceEachIteration, double testFdr,
+                      double selectionFdr, double initialSelectionFdr,
+                      double selectedCpos, double selectedCneg,
+                      unsigned int niter, bool usePi0,
+                      unsigned int nestedXvalBins, bool trainBestPositive,
+                      unsigned int numThreads, bool skipNormalizeScores)
+        : CrossValidation(quickValidation, reportPerformanceEachIteration,
+                          testFdr, selectionFdr, initialSelectionFdr,
+                          selectedCpos, selectedCneg, niter, usePi0,
+                          nestedXvalBins, trainBestPositive, numThreads,
+                          skipNormalizeScores) { }
+    virtual ~CrossValidationEx() { }
+    int doStepEx(bool updateDOC, Normalizer* pNorm, double selectionFdr) {
+        return doStep(updateDOC, pNorm, selectionFdr);
+    }
+    std::vector< std::vector<double> > const& weights(void) const {
+        return w_;
+    }
+};
+
+/*
+ * Unit tests for the CrossValidation class.
+ *
+ * Note: These tests take over 250 ms to complete (on my computer).
+ * They are okay here as long as these are the only slow tests in the
+ * unit test suite. But at some point we will probably want to move
+ * this class into a separate suite of integration tests.
+ */
+
+class CrossValidationTest : public ::testing::Test {
+  protected:
+    virtual void SetUp();
+    virtual void TearDown();
+    int origVerbose;
+};
+
+void CrossValidationTest::SetUp()
+{
+    origVerbose = Globals::getInstance()->getVerbose();
+    Globals::getInstance()->setVerbose(0);
+}
+
+void CrossValidationTest::TearDown()
+{
+    Globals::getInstance()->setVerbose(origVerbose);
+}
+
+TEST_F(CrossValidationTest, doStepTest)
+{
+    // Note that we use an elevated testFdr (0.02 instead of 0.01),
+    // to compensate for the small size of the data sets.
+    int const N = 100;
+    double const testFdr = 0.02;
+
+    CrossValidationEx *crossValidation =
+            new CrossValidationEx(false,   // quickValidation
+                                  false,   // reportEachIteration
+                                  testFdr, // testFdr
+                                  0.01,    // selectionFdr
+                                  0.01,    // initialSectionFdr
+                                  0.0,     // selectedCpos
+                                  0.0,     // selectedCneg
+                                  10,      // nIter
+                                  true,    // usePi0
+                                  1,       // nestedXvalBins
+                                  false,   // trainBestPositive
+                                  1,       // numThreads
+                                  false);  // skipNormalizeScores
+
+    FeatureNames::setNumFeatures(2);
+    SetHandler setHandler(0);
+    int scanNumber = 1;
+
+    // Our data set has two features. One flips between 0 and 1
+    // without regard for label. The other is consistently increasing,
+    // but with the targets' values slightly less than the decoys'
+    // values.
+
+    DataSet *targets = new DataSet();
+    DataSet *decoys = new DataSet();
+    targets->setLabel(+1);
+    decoys->setLabel(-1);
+    for (int i = 0 ; i < 2 * N ; ++i) {
+        PSMDescription *psm = new PSMDescription();
+        psm->features = new double[2];
+        psm->features[0] = static_cast<double>(i) / N - 1E-6;
+        psm->features[1] = static_cast<double>(i % 2);
+        psm->scan = scanNumber++;
+        targets->registerPsm(psm);
+    }
+    for (int i = 0 ; i < N ; ++i) {
+        PSMDescription *psm = new PSMDescription();
+        psm->features = new double[2];
+        psm->features[0] = static_cast<double>(i) / N;
+        psm->features[1] = static_cast<double>(i % 2);
+        psm->scan = scanNumber++;
+        decoys->registerPsm(psm);
+    }
+    setHandler.push_back_dataset(targets);
+    setHandler.push_back_dataset(decoys);
+
+    Normalizer *pNorm = NULL;
+    setHandler.normalizeFeatures(pNorm);
+
+    Scores scores(true);
+    scores.populateWithPSMs(setHandler);
+
+    // The pre-iteration setup should select the first feature to
+    // be weighted.
+
+    SanityCheck *pCheck = new SanityCheck();
+    int numInit = crossValidation->preIterationSetup(scores, pCheck, pNorm,
+                                                setHandler.getFeaturePool());
+    std::vector< std::vector<double> > const& w = crossValidation->weights();
+    EXPECT_EQ(1.0, w[0][0]);
+    EXPECT_EQ(0.0, w[0][1]);
+
+    // One step of the training algorithm should find N positives
+    // (plus some more, due to the elevated false detection rate).
+
+    int numIter = crossValidation->doStep(true, pNorm, 0.01);
+    EXPECT_EQ(N * (1.0 + testFdr), numIter);
+
+    delete crossValidation;
+}
