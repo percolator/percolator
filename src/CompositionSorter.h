4ef21dd2f323bef002b80a5a3a2a50f3c2ac7628
Matthew The <matthew.the@tum.de>
2024-08-20 13:11:31 +0200

    linting CompositionSorter

--- src/CompositionSorter.h
+++ src/CompositionSorter.h
@@ -1,33 +1,44 @@
 #include <iostream>
-#include <string>
 #include <map>
+#include <string>
 #include <unordered_map>
 #include <vector>
 
 // based on the djb2 algorithm
 struct DJB2Hash {
-    size_t operator()(const std::string& s) const {
-        unsigned long hash = 5381;
-        for (char c : s) {
-            hash = ((hash << 5) + hash) + c;
-        }
-        return hash;
+  size_t operator()(const std::string& s) const {
+    unsigned long hash = 5381;
+    for (char c : s) {
+      hash = ((hash << 5) + hash) + c;
     }
+    return hash;
+  }
 };
 
-class ScoreHolder ;
+class ScoreHolder;
 class Scores;
 
 class CompositionSorter {
-    public:
-        // int addPSMs(Scores& psms, bool useTDC=true); 
-        int addPSMs(const Scores& psms, bool useTDC=false); 
-        std::string generateCompositionSignature(const std::string& peptide);
-        int sortScorePerPeptide();
-        int inCompositionCompetition(Scores& bestScoreHolders, unsigned int decoysPerTarget=1);
-        int psmAndPeptide(const Scores& scores, Scores& winnerPeptides, unsigned int decoysPerTarget=1);
-        static void psmsOnly(const Scores& scores, Scores& winnerPeptides);
-        static void retainRepresentatives(const Scores &psms, Scores &winnerPeptides, double selectionFDR, unsigned int decoysPerTarget, bool useCompositionMatch);
-    protected:
-        std::unordered_map<std::string, std::map<std::string,std::vector<ScoreHolder*>>, DJB2Hash> compositionToPeptidesToScore_;
+ public:
+  // int addPSMs(Scores& psms, bool useTDC=true);
+  int addPSMs(const Scores& psms, bool useTDC = false);
+  std::string generateCompositionSignature(const std::string& peptide);
+  int sortScorePerPeptide();
+  int inCompositionCompetition(Scores& bestScoreHolders,
+                               unsigned int decoysPerTarget = 1);
+  int psmAndPeptide(const Scores& scores,
+                    Scores& winnerPeptides,
+                    unsigned int decoysPerTarget = 1);
+  static void psmsOnly(const Scores& scores, Scores& winnerPeptides);
+  static void retainRepresentatives(const Scores& psms,
+                                    Scores& winnerPeptides,
+                                    double selectionFDR,
+                                    unsigned int decoysPerTarget,
+                                    bool useCompositionMatch);
+
+ protected:
+  std::unordered_map<std::string,
+                     std::map<std::string, std::vector<ScoreHolder*>>,
+                     DJB2Hash>
+      compositionToPeptidesToScore_;
 };
