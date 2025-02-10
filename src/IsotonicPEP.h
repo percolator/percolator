14c7d778aa1f05bb5a8311b83bc7870af8a269ca
percolator <lukas.kall@scilifelab.se>
2025-02-10 20:53:40 +0100

    Small edits

--- src/IsotonicPEP.h
+++ src/IsotonicPEP.h
@@ -1,5 +1,5 @@
 /*******************************************************************************
- Copyright 2024 Lukas Käll <lukas.kall@scilifelab.se>
+ Copyright 2025 Lukas Käll <lukas.kall@scilifelab.se>
 
  Licensed under the Apache License, Version 2.0 (the "License");
  you may not use this file except in compliance with the License.
